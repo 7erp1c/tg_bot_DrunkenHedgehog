ec8652e8b2178c5ed2dd0d25767b131a1a907d6b
7erp1c <ul_tray@bk.ru>
2025-01-24 06:20:00 +0300

    fix prettier, buttons eslint

--- src/app.buttons.ts
+++ src/app.buttons.ts
@@ -11,3 +11,13 @@ export const actionButtons: Markup<ReplyKeyboardMarkup> = Markup.keyboard(
     columns: 2,
   },
 );
+// // @ts-ignore
+// export const actionButtonsAdmin: Markup<ReplyKeyboardMarkup> = Markup.keyboard(
+//   [
+//     Markup!.button.callback('Регистрация😨', 'register'),
+//     Markup!.button.callback('Инфа о пользователях', 'users'),
+//   ],
+//   {
+//     columns: 1,
+//   },
+// );
